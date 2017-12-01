623638e2edc86fab587d2def450b4bd249ac7666
RoxaneH <roxane.housen@gmail.com>
2017-12-01 19:24:21 +0100

    Add files via upload

--- Q3c.m
+++ Q3c.m
@@ -1,27 +1,26 @@
 function[med50_wine, biais_med50, var_med50, moy50_wine, biais_mx50, var_mx50] = Q3c(ech50, wine)
 
-    moy50_wine = zeros(100,1);
-    med50_wine = zeros(100,1);
+    wine50_mx = zeros(100,1);
+    wine50_med = zeros(100,1);
     
     for i = 1 : 100
-        ech_i = ech50(i, :, 1);
-        moy50_wine(i,1) = mean(ech_i);
+        ech_i = ech50(:, 1, i);
+        wine50_mx(i,1) = mean(ech_i);
     end
     
-    var_mx_wine = var(moy50_wine);
-    esp = mean(moy50_wine(:));
-    
-    %consommation moyenne de vin pour la population
+    var_mx50 = var(wine50_mx);
+    moy50_wine = mean(wine50_mx(:));
     moy_pop = mean(wine);
     
-    biais_mx_wine =  esp - moy_pop;
+    biais_mx50 =  moy50_wine - moy_pop;
     
     for i = 1 : 100
-        ech_i = ech50(i, :, 1);
-        med50_wine(i,1) = median(ech_i);
+        ech_i = ech50(:, 1, i);
+        wine50_med(i,1) = median(ech_i);
     end
     
-    var_med_wine = var(med50_wine(:));
-    biais_med_wine = mean(med50_wine(:)) - moy_pop;
+    med50_wine = mean(wine50_med(:));
+    var_med50 = var(wine50_med(:));
+    biais_med50 = med50_wine - moy_pop;
     
 end
