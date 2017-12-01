935e5bc79760468667ff2f6bea3ecab860ba1476
RoxaneH <roxane.housen@gmail.com>
2017-12-01 13:57:04 +0100

    Add files via upload
    Modification des indices tmp dans echantillons.m : éviter d'aller à l'indice 101, et éviter le cas de l'indice 0
    Du coup echantillons_100.m fonctionne et je l'ai modifié

--- echantillons.m
+++ echantillons.m
@@ -1,4 +1,5 @@
 function [ech] = echantillons(data)
+
 n_features = size(data, 2);
 nbr_elem = size(data, 1);
 
@@ -6,6 +7,11 @@ ech = zeros(20,n_features);
 
     for i = 1 : 20
        tmp = round(rand * nbr_elem ) + 1;
+       if tmp == 0
+           tmp = tmp +1;
+       elseif tmp == 101
+           tmp = tmp -1 ;
+       end
        for j = 1 : n_features
             ech(i,j) = data(tmp,j);
        end
